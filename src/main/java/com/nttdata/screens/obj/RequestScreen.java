08deeaab656a055d3657925d36946c019f4645d0
pkallair <pkallair@emeal.nttdata.com>
2022-11-23 12:47:05 +0100

    Clases actualizadas

--- src/main/java/com/nttdata/screens/obj/RequestScreen.java
+++ src/main/java/com/nttdata/screens/obj/RequestScreen.java
@@ -2,5 +2,22 @@ package com.nttdata.screens.obj;
 
 public class RequestScreen {
 	ScreenData screenData;
+
+	public RequestScreen() {
+		
+	}
 	
+	public RequestScreen(ScreenData screenData) {
+		super();
+		this.screenData = screenData;
+	}
+
+	public ScreenData getScreenData() {
+		return screenData;
+	}
+
+	public void setScreenData(ScreenData screenData) {
+		this.screenData = screenData;
+	}
+		
 }
--- src/main/java/com/nttdata/screens/obj/ResponseScreen.java
+++ src/main/java/com/nttdata/screens/obj/ResponseScreen.java
@@ -3,4 +3,27 @@ package com.nttdata.screens.obj;
 public class ResponseScreen {
 	String message; 
 	Object userData;
+	
+	public ResponseScreen() {
+		
+	}
+	public ResponseScreen(String message, Object userData) {
+		super();
+		this.message = message;
+		this.userData = userData;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public Object getUserData() {
+		return userData;
+	}
+	public void setUserData(Object userData) {
+		this.userData = userData;
+	}
+	
+	
 }

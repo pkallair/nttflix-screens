7e8c913c4e120e9400b277b7f0ceec43337e5f18
pkallair <pkallair@emeal.nttdata.com>
2022-11-23 12:35:00 +0100

    quinto commit

--- src/main/java/com/nttdata/screens/obj/RequestScreen.java
+++ src/main/java/com/nttdata/screens/obj/RequestScreen.java
@@ -1,5 +1,6 @@
 package com.nttdata.screens.obj;
 
 public class RequestScreen {
-
+	ScreenData screenData;
+	
 }
--- src/main/java/com/nttdata/screens/obj/ResponseScreen.java
+++ src/main/java/com/nttdata/screens/obj/ResponseScreen.java
@@ -1,5 +1,6 @@
 package com.nttdata.screens.obj;
 
 public class ResponseScreen {
-
+	String message; 
+	Object userData;
 }

0537548837dc906fc27c1a9ea1fa7e4fbe4b4754
pkallair <pkallair@emeal.nttdata.com>
2022-11-23 08:26:33 +0100

    Primer commit

---
+++ src/main/java/com/nttdata/screens/NttflixScreensApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.screens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NttflixScreensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NttflixScreensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/screens/NttflixScreensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.screens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NttflixScreensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
